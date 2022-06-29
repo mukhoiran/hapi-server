f2dc5be55f072e6f3c517872b6fd8f4184fa3bec
mukhoiran <mkhoir.anam@gmail.com>
2022-06-29 08:51:04 +0700

    routing for unknown url

--- routes.js
+++ routes.js
@@ -7,10 +7,31 @@ const routes = [
        },
    },
    {
+       method: '*',
+       path: '/',
+       handler: (request, h) => {
+           return 'Page can not access with that method';
+       },
+   },
+   {
        method: 'GET',
        path: '/about',
        handler: (request, h) => {
            return 'About page';
+       },
+   },
+   {
+       method: '*',
+       path: '/about',
+       handler: (request, h) => {
+           return 'Page can not access with that method';
+       },
+   },
+   {
+       method: '*',
+       path: '/{any*}',
+       handler: (request, h) => {
+           return 'Page not found';
        },
    },
 ];
